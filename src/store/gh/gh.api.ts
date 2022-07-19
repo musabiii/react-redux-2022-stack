010ca0d643798bee36f69d2282e464954b792104
Musabii Mambetov <musabi@live.com>
2022-07-19 18:35:57 +0300

    search input

--- src/store/gh/gh.api.ts
+++ src/store/gh/gh.api.ts
@@ -1,15 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IUser, ServerResponse } from "../../models/models";
 export const ghApi = createApi({
   reducerPath: "gh/api",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.github.com/" }),
   endpoints: (build) => ({
-    searchUsers: build.query<any,string>({
+    searchUsers: build.query<IUser[],string>({
       query: (search:string) => ({
         url: `search/users`,
         params:{
-          q:search
+          q:search,
+          per_page:10
         }
       }),
+      //transformResponse - tranforme data from response
+      transformResponse:(response:ServerResponse)=> response.items,
     }),
   }),
 });
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { ghApi } from "./gh/gh.api";
 
-export const store = configureStore({
-    reducer:{
-        [ghApi.reducerPath]:ghApi.reducer
-    }
-})
+export const store = configureStore(
+  {
+    reducer: {
+      [ghApi.reducerPath]: ghApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(ghApi.middleware),
+      devTools:true
+  },
+);
